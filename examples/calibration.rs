c6522c227aa317eac550a334208cc64fea1102fb
pvillela <pvillela@gmail.com>
2025-06-27 12:07:47 -0400

    Enhanced calibration example.

--- examples/calibration.rs
+++ examples/calibration.rs
@@ -11,10 +11,24 @@ use std::time::Duration;
 
 fn main() {
     let target_latency = Duration::from_nanos(2000);
+    let target_latency_nanos = target_latency.as_nanos() as f64;
     let target_effort = calibrate_busy_work(target_latency);
-    let latency = latency(|| busy_work(target_effort));
-    let latency_nanos = latency.as_nanos();
-    println!("target_effort={}", target_effort);
+
     println!("target_latency_nanos={}", target_latency.as_nanos());
-    println!("latency_nanos={}", latency_nanos);
+    println!("target_effort={}", target_effort);
+
+    const N: usize = 10;
+
+    let mut sum2dev = 0.;
+    for _ in 0..N {
+        let latency_nanos = latency(|| busy_work(target_effort)).as_nanos() as f64;
+        sum2dev += (latency_nanos - target_latency_nanos).powi(2);
+
+        println!("latency_nanos={}", latency_nanos);
+    }
+
+    let stdev = (sum2dev / N as f64).sqrt();
+    let rel_stdev = stdev / target_latency_nanos;
+
+    println!("stdev={stdev}, rel_stdev={rel_stdev}");
 }
