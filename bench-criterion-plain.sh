af3b20b0be8296401673b9b285d6bf3301cf90d1
pvillela <pvillela@gmail.com>
2025-07-23 12:22:44 -0400

    Changed bench-criterion-plain.sh to use cargo-criterion.

--- bench-criterion-plain.sh
+++ bench-criterion-plain.sh
@@ -5,7 +5,7 @@ export RUSTFLAGS="-Awarnings"
 echo "----- Started: `date +"%Y-%m-%d at %H:%M:%S"` -----"
 echo
 
-cargo bench --bench criterion_plain --features _dev_utils,criterion --target-dir target/bench-target
+cargo criterion --bench criterion_plain --features _dev_utils,criterion --target-dir target/bench-target --message-format=json
 
 echo ""
 echo "Finished at: `date +"%H:%M:%S"`"
