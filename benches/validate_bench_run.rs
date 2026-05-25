9c603bc02ad871e6ab38ec289a78a909de88c014
pvillela <pvillela@gmail.com>
2026-05-25 11:49:48 -0400

    Added approximate equality support for Duration in test_support and cleaned-up benches/validate_bench_run.rs.

--- benches/validate_bench_run.rs
+++ benches/validate_bench_run.rs
@@ -1,5 +1,7 @@
-use basic_stats::{dev_utils::ApproxEq, rel_approx_eq};
-use bench_utils::{BenchCfg, RunLength, bench_run_with_status_arg_cfg};
+use bench_utils::{
+    BenchCfg, RunLength, bench_run_with_status_arg_cfg, rel_approx_eq_dur,
+    test_support::AbsRelDiffDur,
+};
 use std::{thread, time::Duration};
 
 const EPSILON: f64 = 0.005;
@@ -10,9 +12,7 @@ fn sleep_fn(target_latency: Duration) {
 }
 
 fn run_bench(name: &'static str, warmup_millis: u64, target_latency: Duration, check: bool) {
-    let reporting_unit = BenchCfg::default().reporting_unit();
-    let target_median = reporting_unit.latency_as_f64(target_latency);
-    let exec_count = (reporting_unit.latency_as_f64(BENCH_TIME) / target_median) as usize;
+    let exec_count = (BENCH_TIME.as_secs_f64() / target_latency.as_secs_f64()) as usize;
     let cfg = BenchCfg::default().with_warmup_millis(warmup_millis);
     println!("validate_bench_run: {name}");
     let out = bench_run_with_status_arg_cfg(
@@ -20,16 +20,16 @@ fn run_bench(name: &'static str, warmup_millis: u64, target_latency: Duration, c
         || sleep_fn(target_latency),
         RunLength::Count(exec_count),
     );
+    let out_median = out.median();
     println!(
-        "target_median={target_median}, out.median()={}, rel_diff={}",
-        out.median(),
-        target_median.abs_rel_diff(out.median())
+        "target_median={target_latency:?}, out.median()={out_median:?}, rel_diff={}",
+        target_latency.abs_rel_diff(out_median)
     );
     println!("{:?}", out.summary());
     println!();
 
     if check {
-        rel_approx_eq!(target_median, out.median(), EPSILON);
+        rel_approx_eq_dur!(target_latency, out_median, EPSILON);
     }
 }
 
@@ -38,5 +38,5 @@ fn main() {
     run_bench("sleep_60_millis", 600, Duration::from_millis(60), true);
 
     // short sleep, very noisy
-    run_bench("sleep_60_micros", 100, Duration::from_micros(60), false);
+    run_bench("sleep_60_micros", 100, Duration::from_micros(60), true);
 }
