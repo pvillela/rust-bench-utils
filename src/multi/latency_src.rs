3552979a011a07334bb1a759d16a3e1b513b8589
pvillela <pvillela@gmail.com>
2026-06-03 16:34:52 -0400

    Inlined LatencySrcX next() methods.

--- src/multi/latency_src.rs
+++ src/multi/latency_src.rs
@@ -17,6 +17,7 @@ pub struct LatencySrc1<F0: FnMut()>(pub F0);
 impl<F0: FnMut()> Iterator for LatencySrc1<F0> {
     type Item = [Duration; 1];
 
+    #[inline(always)]
     fn next(&mut self) -> Option<Self::Item> {
         Some([latency(&mut self.0)])
     }
@@ -33,6 +34,7 @@ pub struct LatencySrc2<F0: FnMut(), F1: FnMut()>(pub F0, pub F1);
 impl<F0: FnMut(), F1: FnMut()> Iterator for LatencySrc2<F0, F1> {
     type Item = [Duration; 2];
 
+    #[inline(always)]
     fn next(&mut self) -> Option<Self::Item> {
         Some([latency(|| self.0()), latency(|| self.1())])
     }
